2048f6daa6de3f6e3a80b3c6d0213538335de9d5
Dairon Medina Caro <dairon@gooee.com>
2017-01-25 12:08:19 -0500

    Added HTTP methods to client

--- gooee/client.py
+++ gooee/client.py
@@ -4,16 +4,25 @@ from platform import platform
 
 import requests
 
-from .compat import json, string_type
+from .compat import json
 from .exceptions import (
     IllegalHttpMethod,
 )
 from . import __version__
+from .utils import (
+    format_path,
+    GOOEE_API_URL
+)
 
 
 class Gooee(object):
+    """Gooee HTTP client class."""
 
     allowed_methods = ['post', 'get', 'delete']
+
+    def __init__(self, oauth_token, api_base_url=GOOEE_API_URL):
+        self.oauth_token = oauth_token
+        self.api_base_url = api_base_url
 
     @property
     def headers(self):
@@ -26,3 +35,29 @@ class Gooee(object):
             )
         }
         return headers
+
+    def api(self, method, path, data):
+        method = method.strip().lower()
+        if method not in self.allowed_methods:
+            msg = "The '{0}' method is not accepted by Gooee SDK.".format(method)
+            raise IllegalHttpMethod(msg)
+        method = getattr(self, method)
+        return method(path, data)
+
+    def get(self, path, data=None):
+        # Get the function path
+        path = format_path(path, self.api_base_url)
+
+        if data is None:
+            data = {}
+
+        return requests.get(path, headers=self.headers, params=data or {})
+
+    def post(self, path, data=None):
+        path = format_path(path, self.api_base_url)
+        json_data = json.dumps(data or {})
+        return requests.post(path, headers=self.headers, data=json_data)
+
+    def delete(self, path, data=None):
+        path = format_path(path, self.api_base_url)
+        return requests.delete(path, headers=self.headers, data=data or {})
