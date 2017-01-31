b59d0d764e5fa5128bb1561300cd6bcae502f19a
Dairon Medina Caro <dairon@gooee.com>
2017-01-30 22:29:41 -0500

    Added license headers to all the files

--- gooee/compat.py
+++ gooee/compat.py
@@ -1,5 +1,16 @@
 # -*- coding: utf-8 -*-
-
+# Copyright 2017 Gooee.com, LLC. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You
+# may not use this file except in compliance with the License. A copy of
+# the License is located at:
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# or in the "LICENSE" file accompanying this file. This file is
+# distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+# ANY KIND, either express or implied. See the License for the specific
+# language governing permissions and limitations under the License.
 import sys
 
 PY3 = sys.version_info[0] == 3
