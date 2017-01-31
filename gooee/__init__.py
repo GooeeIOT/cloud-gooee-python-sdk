0a13555279c72e94fe7a5815a9be7781b5b3c5a0
Dairon Medina Caro <dairon@gooee.com>
2017-01-30 22:39:44 -0500

    Added module logging handlers

--- gooee/__init__.py
+++ gooee/__init__.py
@@ -13,9 +13,46 @@
 # language governing permissions and limitations under the License.
 from __future__ import unicode_literals
 
+import logging
+
 __author__ = 'Gooee LLC'
 __email__ = 'dairon@gooee.com'
 __version__ = '0.0.1'
 
-
 from .client import Gooee  # noqa
+
+
+def set_stream_logging(level=logging.DEBUG, format_string=None):
+    """
+    Add a stdout log handler with provided level to the SDK operations.
+        >>> import logging
+        >>> import gooee
+        >>> gooee.set_stream_logging(logging.INFO)
+    :type level: int
+    :param level: Logging level, e.g. ``logging.INFO``
+    :type format_string: str
+    :param format_string: Log message format
+    """
+    if format_string is None:
+        format_string = "%(asctime)s %(name)s [%(levelname)s] %(message)s"
+
+    logger = logging.getLogger('gooee')
+    logger.setLevel(level)
+    handler = logging.StreamHandler()
+    handler.setLevel(level)
+    formatter = logging.Formatter(format_string)
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
+
+class NullHandler(logging.Handler):
+    """
+    Set up logging to ``/dev/null`` like a library is supposed to.
+    http://docs.python.org/3.3/howto/logging.html#configuring-logging-for-a-library
+    """
+
+    def emit(self, record):
+        pass
+
+
+logging.getLogger('gooee').addHandler(NullHandler())
